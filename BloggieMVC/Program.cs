8523584e30063ff8108f94809e1016b4184987e4
Paweł <84644073+Pawel-Lechki@users.noreply.github.com>
2024-03-18 09:52:10 +0100

    Add models and DbContext

--- BloggieMVC/Program.cs
+++ BloggieMVC/Program.cs
@@ -1,7 +1,12 @@
+using BloggieMVC.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<BloggieDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BloggieDbConnString")));
 
 var app = builder.Build();
 
