f88f87bfdf4f961a6af0353ae55c7743d93898db
Paweł <84644073+Pawel-Lechki@users.noreply.github.com>
2024-03-18 10:29:08 +0100

    Add addTages to database functionality

--- BloggieMVC/Controllers/AdminTagsController.cs
+++ BloggieMVC/Controllers/AdminTagsController.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BloggieMVC.Data;
+using BloggieMVC.Models.Domain;
+using BloggieMVC.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BloggieMVC.Controllers;
 
 public class AdminTagsController : Controller
 {
+    private readonly BloggieDbContext _dbContext;
+
+    public AdminTagsController(BloggieDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
     [HttpGet]
     public IActionResult Add()
     {
         return View();
+    }
+    [HttpPost]
+    [ActionName("Add")]
+    public IActionResult SubmitTag(AddTagRequest addTagRequest)
+    {
+        var tag = new Tag
+        {
+            Name = addTagRequest.Name,
+            DisplayName = addTagRequest.DisplayName
+        };
+        
+        _dbContext.Tags.Add(tag);
+        _dbContext.SaveChanges();
+        
+        return View("Add");
     }
 }
---
+++ BloggieMVC/Models/ViewModels/AddTagRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace BloggieMVC.Models.ViewModels;
+
+public class AddTagRequest
+{
+    public string Name { get; set; }
+    public string DisplayName { get; set; }
+}
