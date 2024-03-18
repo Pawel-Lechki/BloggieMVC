109b37ee72fb3fa9d9e5ee753c302032e5b3d883
Paweł <84644073+Pawel-Lechki@users.noreply.github.com>
2024-03-18 10:08:49 +0100

    Add new view AdminTags Add

---
+++ BloggieMVC/Controllers/AdminTagsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BloggieMVC.Controllers;
+
+public class AdminTagsController : Controller
+{
+    [HttpGet]
+    public IActionResult Add()
+    {
+        return View();
+    }
+}
